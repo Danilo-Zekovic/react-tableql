88a4f805bdfa892bafe2ec73244d52dbe5c66381
Danilo-Zekovic <danilozeka93@gmail.com>
2020-07-29 20:53:55 +0200

    bundle for node

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
-module.exports = {
+const nodeExternals = require('webpack-node-externals')
+
+const web = {
   mode: 'production',
   entry: './src/index.ts',
   output: {
@@ -42,3 +44,53 @@ module.exports = {
     'graphql-tag': 'commonjs graphql-tag',
   },
 }
+
+const node = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.node.js',
+    libraryTarget: 'commonjs2',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx'],
+  },
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.ts$|.tsx$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  externals: [
+    nodeExternals(),
+    {
+      react: 'commonjs react', // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
+      'react-apollo': 'commonjs react-apollo',
+      graphql: 'commonjs graphql',
+      'graphql-tag': 'commonjs graphql-tag',
+    },
+  ],
+}
+
+module.exports = [web, node]
