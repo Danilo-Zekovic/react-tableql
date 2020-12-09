b56b7784395a7d25ac2e8fa763d313ac28f8332d
Danilo-Zekovic <danilozeka93@gmail.com>
2020-12-09 21:00:44 +0100

    new storybook build with storybook v6

---
+++ docs/main.71fa9011c3537d971d08.bundle.js
@@ -0,0 +1,21 @@
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [0],
+  {
+    1600: function (module, exports, __webpack_require__) {
+      __webpack_require__(1601),
+        __webpack_require__(1779),
+        __webpack_require__(2584),
+        __webpack_require__(2681),
+        __webpack_require__(2692),
+        __webpack_require__(2730),
+        __webpack_require__(2739),
+        (module.exports = __webpack_require__(2787))
+    },
+    1675: function (module, exports) {},
+    2787: function (module, exports, __webpack_require__) {
+      'use strict'
+      __webpack_require__(164).addons.setConfig({ refs: {} })
+    },
+  },
+  [[1600, 1, 2]],
+])
