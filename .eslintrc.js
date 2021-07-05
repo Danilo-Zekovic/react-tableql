9930ddb74950ef341fb5f44e4499afb41aace97a
Danilo-Zekovic <danilozeka93@gmail.com>
2021-07-05 15:31:04 +0200

    add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'react/prop-types': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+  },
+}
--- lib/react-tableql/src/index.tsx
+++ lib/react-tableql/src/index.tsx
@@ -1,11 +1,23 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 
 interface FooProps {
   foo: string
 }
 
 const Foo: FC<FooProps> = ({ foo }) => {
-  return <div>Hello World! {foo} is the best, but you knew that already</div>
+  const halo = (): void => {
+    console.log('foo bar')
+  }
+
+  useEffect(() => {
+    console.log('hi', foo)
+  }, [foo])
+
+  return (
+    <div onClick={halo}>
+      Hello World! {foo} is the best, but you knew that already.
+    </div>
+  )
 }
 
 export default Foo
