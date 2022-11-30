e3178b5cf8012e7fa053483e0aeaff7936eb693d
Danilo-Zekovic <danilozeka93@gmail.com>
2022-11-30 14:02:47 +0100

    cra move to react 18

--- examples/create-react-app-typescript/src/index.tsx
+++ examples/create-react-app-typescript/src/index.tsx
@@ -1,18 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import { TableQLProvider } from 'react-tableql'
 
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+root.render(
   <React.StrictMode>
     <TableQLProvider theme={{ primary: 'tomato' }}>
       <App />
     </TableQLProvider>
   </React.StrictMode>,
-  document.getElementById('root'),
 )
 
 // If you want to start measuring performance in your app, pass a function
